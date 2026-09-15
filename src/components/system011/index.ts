4ec37e3a13d020702d33ca6abaedfb5ca446d34d
klsjnh <xiangrkrs@163.com>
2026-09-15 21:18:56 +0800

    ver 0.0.29 ...

--- src/components/system011/index.ts
+++ src/components/system011/index.ts
@@ -6,5 +6,7 @@ export { KlsjnhSql011 } from './KlsjnhSql011';
 export type { KlsjnhSql011Props } from './KlsjnhSql011';
 export { KlsjnhMarkdown011 } from './KlsjnhMarkdown011';
 export type { KlsjnhMarkdown011Props } from './KlsjnhMarkdown011';
+export { TestFeedbackAlert } from './TestFeedbackAlert';
+export type { TestFeedback } from './TestFeedbackAlert';
 export { formatSQL, compressSQL, tryFormatSQL } from '@/utils/system011/Sql011Util';
 export type { SqlFormatLanguage, SqlFormatMode } from '@/utils/system011/Sql011Util';
